d5bade1766fc70438f6e38601297c1f289ab84c6
mohit choudhary <mohitchoudhary1054@gmail.com>
2023-08-06 16:07:07 +0530

    Level 5 authentication cookies and session

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,133 +1,113 @@
-//jshint esversion:6
-
-// requiring dotenv for keepting aur encryption key and api key
-require("dotenv").config();
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-//for level 3 authentication
-//const md5 = require('md5');
-
-// for level 2 authentication
-// const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// Connecting mongoDB with the node sever
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+mongoose.connection.on("connected",()=>{
+    console.log("connected");
+})
 
-// Event listeners to check connection status
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.log("Disconnected from MongoDB");
-});
-
-//Creating a Schema for users
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema ({
   email: String,
-  password: String,
+  password: String
 });
 
-// defining secret for encrption
-// defined inside .env file for security
-// console.log(process.env.SECRET)
+userSchema.plugin(passportLocalMongoose);
 
-//we must specify the plugin before we create mongoose model for level 2 authentication
-//userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"]});
-
-// Setting up new user model
 const User = new mongoose.model("User", userSchema);
 
-// Handling initial route
+passport.use(User.createStrategy());
 
-app.get("/", (req, res) => {
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res){
   res.render("home");
 });
 
-// Handling login route
-
-app.get("/login", (req, res) => {
+app.get("/login", function(req, res){
   res.render("login");
 });
 
-// Handling register route
-
-app.get("/register", (req, res) => {
+app.get("/register", function(req, res){
   res.render("register");
 });
 
-// Catching the register request
-app.post("/register",(req, res) => {
-  // creating a new user with the entered value
-  try {
-
-    bcrypt.hash(req.body.password,saltRounds,async function(err,hash){
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-          });
-          const savedUser = await newUser.save();
-          res.render("secrets");
-          console.log("User registered successfully:", savedUser);
-    });
-
-    
-  } catch (error) {
-    console.error("Error registering user:", error);
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
   }
 });
 
-//Handelling request for login route
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
 
-app.post("/login", async (req, res) => {
-  // Accessing the credentials
-  const username = req.body.username;
-  const password = req.body.password;
+app.post("/register", function(req, res){
 
-  // checking the credentials against our database using the find method of mongoose
-  try {
-    const foundUser = await User.findOne({
-      email: username,
-    });
-    if (foundUser) {
-      console.log(` ${foundUser}`);
-      bcrypt.compare(password,foundUser.password,(err,result)=>{
-
-        if(result=== true){
-            res.render('secrets');
-        }
-
-
-      });
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
     } else {
-      console.log("No such User");
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
-  } catch (error) {
-    console.log(`Error finding user : ${error}`);
-  }
+  });
+
 });
 
-app.listen(3000, () => {
-  console.log(`Server started on port: 3000`);
+app.post("/login", function(req, res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
 });
